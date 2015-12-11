42244ff933b5dd6f86eb2906319667f62e2986a9
Joe Cora <joseph.cora@nytimes.com>
2015-12-11 14:56:20 -0500

    League frontend changes

--- frontend/league.js
+++ frontend/league.js
@@ -1,58 +1,125 @@
 (function() {
-	'use strict';
-	
-	// Angular setup
-	var moduleNYT = angular.module('NYTFantasy', ['ui.bootstrap']);
-	
-	moduleNYT.controller('NYTFantasyController', NYTFantasyController);
-	moduleNYT.controller('NYTFantasyViewController', NYTFantasyViewController);
-	moduleNYT.service('wordEndpoint', wordEndpoint);
-	
-	// Main controller
-	function NYTFantasyController($scope) {
-		// Displayed view
-		$scope.currentView = 0;
-		
-		// Load today's words
-		$scope.todaysWords = wordEndpoint();
-		
-		angular.forEach($scope.todaysWords, function(word) {
-			if (word.score > 500) {
-				word.rank = 'success';
-			} else if (word.score > 100 && word.score <= 500) {
-				word.rank = 'info';
-			} else {
-				word.rank = 'danger';
-			}
-		});
-	}
-	
-	// View controller
-	function NYTFantasyViewController($scope) {
-		// Displayed view (start with view 1)
-		$scope.currentView = 1;
-		
-		// Menu handler
-		$scope.updateView = function(view_pos) {
-			$scope.currentView = view_pos;
-		};
-		
-		// View visibility handler
-		$scope.showView = function(view_pos) {
-			return $scope.currentView === view_pos;
-		};
-	}
-	
-	// Word API Handler
-	function wordEndpoint($http) {
-		/*return function() {
-			var url = '/data/scores';
-			
-			return $http.get(url).then(function(response) {
-				return response.data;
-			});
-		}*/
-		
-		return [{"word":"Tom", "score":"150"}, {"word":"hope", "score":"30"}, {"word":"time", "score":"520"}];
-	}
+    'use strict';
+
+    // Fake data
+    var league_details = {
+        "id": 2,
+        "name":"Dec 5th League",
+        "status":"CLOSED",
+        "score_time":"2015-12-06T06:00:00Z",
+        "score":34,
+        "rank": 61,
+        "player_count": 2000,
+        "ngrams" : [
+            {
+                "ngram": "President Speech",
+                "score": 5
+            },
+            {
+                "ngram": "Elections",
+                "score": 7
+            },
+            {
+                "ngram": "Tennis Games",
+                "score": 12
+            },
+            {
+                "ngram": "Giants",
+                "score": 7
+            },
+            {
+                "ngram": "France",
+                "score": 3
+            }
+        ],
+        "leader_board":[
+            {
+                "rank": 1,
+                "user_name": "icemaker",
+                "user_id": 59,
+                "score": 85
+            },
+            {
+                "rank": 2,
+                "user_name": "icemaker",
+                "user_id": 101,
+                "score": 82
+            }
+        ]
+    };
+    
+    // Angular setup
+    var moduleNYT = angular.module('NYTFantasy', ['ui.bootstrap']);
+    
+    moduleNYT.controller('NYTFantasyController', NYTFantasyController);
+    moduleNYT.controller('NYTFantasyViewController', NYTFantasyViewController);
+    moduleNYT.factory('getUserLeagueDetailsService', getUserLeagueDetailsService);
+    
+    // Main controller
+    function NYTFantasyController($scope) {
+        // League options
+        $scope.user_id = 1;
+        $scope.league_id = 1;
+        $scope.league_data = null;
+
+        // View options
+        $scope.currentView = 1;
+        
+        // Load the league details for user
+        loadUserLeagueDetails($scope);
+    }
+    
+    // View controller
+    function NYTFantasyViewController($scope) {
+        // Displayed view (start with view 1)
+        $scope.currentView = 1;
+        
+        // Menu handler
+        $scope.updateView = function(view_pos) {
+            $scope.currentView = view_pos;
+        };
+        
+        // View visibility handler
+        $scope.showView = function(view_pos) {
+            return $scope.currentView === view_pos;
+        };
+    }
+
+    // API Service Handlers
+    function loadUserLeagueDetails($scope) {
+        getUserLeagueDetailsService().service($scope.user_id, function(data) {
+            // Store the n-gram results
+            $scope.league_data = data;
+            $scope.league_data.ngrams = data.ngrams;
+
+            angular.forEach($scope.league_data.ngrams, function(ngram) {
+                if (ngram.score > 10) {
+                    ngram.rank = 'success';
+                } else if (ngram.score > 5 && ngram.score <= 10) {
+                    ngram.rank = 'info';
+                } else {
+                    ngram.rank = 'danger';
+                }
+            });
+        });
+    }
+
+    // API Services
+    function getUserLeagueDetailsService($http) {
+        return {
+            service: function(user_id, callback) {
+                callback(league_details);
+            }
+        };
+
+        /*return function() {
+            var url = '/data/scores';
+            
+            return $http.get(url).then(function(response) {
+                return response.data;
+            });
+        }
+        
+        return [{"word":"Tom", "score":"150"}, {"word":"hope", "score":"30"}, {"word":"time", "score":"520"}];*/
+    }
 })();
